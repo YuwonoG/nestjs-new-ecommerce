94d9634d915cc2c1251c0290e9f03e322a15919d
YuwonoG <Yuwono_g@hotmail.com>
2020-10-31 14:50:43 +0700

    Initial

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeOrmConfig : TypeOrmModuleOptions ={
+    type : 'postgres',
+    host : 'localhost',
+    port : 5432,
+    username : 'postgres',
+    password : 'viewsonic',
+    database : 'rbac',
+    entities : [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize : true, 
+    logging : true,
+}
